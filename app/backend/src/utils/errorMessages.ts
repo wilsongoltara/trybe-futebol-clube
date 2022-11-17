7a3f474edfaff0547e9edcc508ef830819915104
wilsongoltara <wilson.goltara15@gmail.com>
2022-11-17 20:03:51 -0300

    feat: add list of errors and status http

---
+++ app/backend/src/utils/errorMessages.ts
@@ -0,0 +1,6 @@
+const errorMessages = {
+  allFields: 'All fileds must be filled',
+  incorrectFields: 'Incorrect email or password',
+};
+
+export default errorMessages;
---
+++ app/backend/src/utils/statusHttp.ts
@@ -0,0 +1,7 @@
+const statusHttp = {
+  badRequest: 400,
+  unauthorized: 401,
+  notFound: 404,
+};
+
+export default statusHttp;
