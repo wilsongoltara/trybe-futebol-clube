e46ba9b88d83f80281ee990b2e1d21fe72e96d54
wilsongoltara <wilson.goltara15@gmail.com>
2022-11-19 10:39:34 -0300

    refactor: formatter code

--- app/backend/src/database/models/UsersModel.ts
+++ app/backend/src/database/models/UsersModel.ts
@@ -9,34 +9,37 @@ class Users extends Model {
   username!: string;
 }
 
-Users.init({
-  id: {
-    type: INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Users.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING(100),
+      allowNull: false,
+    },
+    role: {
+      type: STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: STRING(100),
+      allowNull: false,
+    },
+    password: {
+      type: STRING(100),
+      allowNull: false,
+    },
   },
-  username: {
-    type: STRING(100),
-    allowNull: false,
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'Users',
+    timestamps: false,
   },
-  role: {
-    type: STRING(100),
-    allowNull: false,
-  },
-  email: {
-    type: STRING(100),
-    allowNull: false,
-  },
-  password: {
-    type: STRING(100),
-    allowNull: false,
-  },
-}, {
-  underscored: true,
-  sequelize: db,
-  modelName: 'Users',
-  timestamps: false,
-});
+);
 
 export default Users;
